97baac26b99775507d19625f7deff7ea5239eeb0
kandi <kandibandasaigopi446@gmail.com>
2024-11-15 11:30:50 +0530

    first commit

---
+++ src/test/java/com/neoteric/map/mapdemo/MapdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoteric.map.mapdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
